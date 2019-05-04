791e1cd7ffffd1422b022c3ffa4dcfa24398c041
capstoneadmin <capstoneadmin@pn.local>
2019-05-04 14:59:39 +0800

    deployment update

--- app/routes/Route.js
+++ app/routes/Route.js
@@ -1,8 +1,8 @@
 var router = require('express').Router();
 
-router.use('/user', require('./User'));
+router.use('/user', require('./user'));
 router.use('/admin', require('./Admin'));
-router.use('/breeder', require('./Breeder'));
-router.use('/investor', require('./Investor'));
+router.use('/breeder', require('.breeder'));
+router.use('/investor', require('./investor'));
 
 module.exports = router;
